5c7687971e614f2eac6e4485812d572983f5c8b5
Akshay Varma <akshayvarma0102@gmail.com>
2023-12-25 00:14:57 +0530

    userRoute till middleware completed

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,18 @@
 import express from 'express'
-import {registerContoller} from "../controllers/userController.js";
+import {registerContoller,
+    loginController,
+     testController} from "../controllers/userController.js";
+
+import { requireSignIn } from '../../middlewares/authMiddleware.js';
 
 const router=express.Router();
 //checking
 router.post("/register",registerContoller);
-//
+
+//login
+router.post('/login',loginController)
+
+//test routes
+router.get('test',requireSignIn,isAdmin, testController)
+
 export default router;
